8fa117473a18fa2a35c58ee53102e591ef77b6b0
cad.maxime@gmail.com <cad.maxime@gmail.com@5961f115-92ae-dc64-abee-4488508b7c1c>
2014-07-21 20:11:12 +0000

    Removing Clean Branch Test :)

--- branches/clean/source/gen/fr/alvini/insta/budgetmonitor/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package fr.alvini.insta.budgetmonitor;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- branches/clean/source/src/fr/alvini/insta/budgetmonitor/model/ObjectModel.java
+++
@@ -1,9 +0,0 @@
-package fr.alvini.insta.budgetmonitor.model;
-
-public class ObjectModel {
-
-	public ObjectModel() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
